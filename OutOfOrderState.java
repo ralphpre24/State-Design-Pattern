bf5ffda7d1d29fec107e4072f3eda7717cb9381f
Ralph Nikko Predas <142536413+ralphpre24@users.noreply.github.com>
2025-02-14 15:41:35 +0800

    OutOfOrderState.java

--- OutOfOrderState.java
+++ OutOfOrderState.java
@@ -1,23 +1,21 @@
-public class OutOfOrderState implements VendingMachineState {
+class OutOfOrderState implements VendingMachineState {
     @Override
     public void selectItem(VendingMachine vendingMachine, String item) {
-        System.out.println("Vending machine is out of order. Cannot select item.");
+        System.out.println("Vending machine is out of order. No operations allowed.");
     }
 
     @Override
-    public void insertCoin(VendingMachine vendingMachine, double amount) {
-        System.out.println("Vending machine is out of order. Cannot insert coin.");
+    public void insertCoin(VendingMachine vendingMachine, int amount) {
+        System.out.println("Vending machine is out of order. No operations allowed.");
     }
 
     @Override
     public void dispenseItem(VendingMachine vendingMachine) {
-        System.out.println("Vending machine is out of order. Cannot dispense item.");
+        System.out.println("Vending machine is out of order. No operations allowed.");
     }
 
     @Override
-    public void setOutOfOrder(VendingMachine vendingMachine, boolean outOfOrder) {
-        if (!outOfOrder) {
-            vendingMachine.setCurrentState(new IdleState());
-        }
+    public void setOutOfOrder(VendingMachine vendingMachine) {
+        System.out.println("Vending machine is already out of order.");
     }
 }
