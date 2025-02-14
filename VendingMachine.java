c1a8ec1701fd8daf621f703a5ae15ba04397a555
Ralph Nikko Predas <142536413+ralphpre24@users.noreply.github.com>
2025-02-14 15:42:27 +0800

    VendingMachine.java

---
+++ VendingMachine.java
@@ -0,0 +1,55 @@
+class VendingMachine {
+    private VendingMachineState currentState;
+    private String selectedItem;
+    private int balance;
+    private Map<String, Integer> itemInventory;
+
+    public VendingMachine() {
+        this.currentState = new IdleState();
+        this.balance = 0;
+        this.itemInventory = new HashMap<>();
+        itemInventory.put("Soda", 2);
+        itemInventory.put("Chips", 3);
+        itemInventory.put("Candy", 1);
+    }
+
+    public void setState(VendingMachineState state) {
+        this.currentState = state;
+    }
+
+    public String getSelectedItem() {
+        return selectedItem;
+    }
+
+    public void setSelectedItem(String selectedItem) {
+        this.selectedItem = selectedItem;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getItemPrice(String item) {
+        return itemInventory.getOrDefault(item, 0);
+    }
+
+    public void selectItem(String item) {
+        currentState.selectItem(this, item);
+    }
+
+    public void insertCoin(int amount) {
+        currentState.insertCoin(this, amount);
+    }
+
+    public void dispenseItem() {
+        currentState.dispenseItem(this);
+    }
+
+    public void setOutOfOrder() {
+        currentState.setOutOfOrder(this);
+    }
+}
